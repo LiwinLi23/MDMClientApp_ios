343095b267a31149158c2a882c3affe11cf77b25
Liwin Lee <LiwinLi@icloud.com>
2018-10-23 18:28:31 +0800

    Adjust default page

--- MDMClientApp/Classes/AppDelegate.h
+++ MDMClientApp/Classes/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  MDMClientApp
-//
-//  Created by 李华林 on 14/11/28.
-//  Copyright (c) 2014年 李华林. All rights reserved.
-//
-
 #import "MDMAppDelegate.h"
 
 @interface AppDelegate : MDMAppDelegate
--- MDMClientApp/main.m
+++ MDMClientApp/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  MDMClientApp
-//
-//  Created by 李华林 on 14/11/28.
-//  Copyright (c) 2014年 李华林. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
